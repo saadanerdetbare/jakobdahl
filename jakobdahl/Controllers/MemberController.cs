fa2eddc18df267d802e8d3f65cd25b63fd2bee0e
Jakob Dahl <jakobdahl@gmail.com>
2020-04-23 18:55:33 +0200

    membership login working

---
+++ jakobdahl/Controllers/MemberController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Umbraco.Web.Mvc;
+using jakobdahl.Models;
+
+namespace jakobdahl.Controllers
+{
+	public class MemberController : SurfaceController
+	{
+		public ActionResult RenderLogin()
+		{
+			return PartialView("~/Views/Partials/Login.cshtml", new LoginModel());
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult SubmitLogin(LoginModel model, string returnUrl)
+		{
+			if (ModelState.IsValid)
+			{
+				if (Membership.ValidateUser(model.Username, model.Password))
+				{
+					FormsAuthentication.SetAuthCookie(model.Username, false);
+					UrlHelper myHelper = new UrlHelper(HttpContext.Request.RequestContext);
+					if (myHelper.IsLocalUrl(returnUrl))
+					{
+						return Redirect(returnUrl);
+					}
+					else
+					{
+						return Redirect("/login/");
+					}
+				}
+				else
+				{
+					ModelState.AddModelError("", "The username or password is incorrect");
+				}
+			}
+
+			return CurrentUmbracoPage();
+		}
+
+		public ActionResult RenderLogout()
+		{
+			return PartialView("~/Views/Partials/Logout.cshtml", null);
+		}
+
+		public ActionResult SubmitLogout()
+		{
+			TempData.Clear();
+			Session.Clear();
+			FormsAuthentication.SignOut();
+			return RedirectToCurrentUmbracoPage();
+		}
+	}
+}
